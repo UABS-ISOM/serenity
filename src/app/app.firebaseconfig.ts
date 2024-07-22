b2e42dad40a5f4373a42d9c43e3cfdcc0857a418
shohil-kishore <s.kishore@auckland.ac.nz>
2024-07-23 09:55:08 +1200

    Production

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,10 +1,10 @@
 export var config = {
-  apiKey: "AIzaSyDepb9MIhxiExMsfPnFP9SDPEE8otjSAHI",
-  authDomain: "cleanchat-92927.firebaseapp.com",
-  databaseURL: "https://cleanchat-92927.firebaseio.com",
-  projectId: "cleanchat-92927",
-  storageBucket: "cleanchat-92927.appspot.com",
-  messagingSenderId: "708755834624",
-  appId: "1:708755834624:web:4070f6cf0370d16b3a55e5",
-  measurementId: "G-62PEG8MQGW",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
